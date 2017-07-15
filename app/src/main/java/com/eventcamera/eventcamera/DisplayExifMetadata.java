4d34bd4f15d92dbf0ae3096be6e6030c69c68c38
Teemu Mutanen <teemu@Teemu-MacBook-Pro.local>
2017-07-15 16:53:35 +0300

    exifInterface

--- app/src/main/java/com/eventcamera/eventcamera/DisplayExifMetadata.java
+++ app/src/main/java/com/eventcamera/eventcamera/DisplayExifMetadata.java
@@ -1,14 +1,14 @@
 package com.eventcamera.eventcamera;
 
 import android.content.Intent;
+import android.media.ExifInterface;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.File;
+import java.io.IOException;
 
 public class DisplayExifMetadata extends AppCompatActivity {
 
@@ -26,18 +26,27 @@ public class DisplayExifMetadata extends AppCompatActivity {
 
 
         File storagePath = Environment.getExternalStorageDirectory();
-        File omaKuva = new File(storagePath.toString() + "/101002-beer-garden.jpg");
+        //File omaKuva = new File(storagePath.toString() + "/main_900.jpg");
+        File omaKuva = new File(storagePath.toString() + "/DCIM/Camera/IMG_20170715_151159.jpg");
 
-        String kuvaus = " ";
-        if(omaKuva.exists()){
-            kuvaus = omaKuva.toString();
-        }else{
-            kuvaus = "ei tiedossa";
-        }
+        String kuvaus2 = String.valueOf(omaKuva.exists());
 
         TextView textView = (TextView) findViewById(R.id.metadataView);
-        textView.setText(kuvaus);
+        textView.setText(kuvaus2);
 
+        String kuvaus3 = " ";
+        String kuvaus6 = " ";
+
+        try{
+            ExifInterface exif = new ExifInterface(omaKuva.toString());
+            kuvaus3 = "kuvaus:  " + exif.getAttribute(ExifInterface.TAG_X_RESOLUTION);
+            //kuvaus6 = exif.toString();
+        }catch(IOException e){
+
+        }
+
+        //TextView textView = (TextView) findViewById(R.id.metadataView);
+        //textView.setText(kuvaus3);
 
     }
 }
